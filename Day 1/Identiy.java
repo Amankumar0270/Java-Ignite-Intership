8b01f8a9baf6bf80e602401d035d5714fb5bfc5a
Amankumar0270 <cse.25bcsi39@silicon.ac.in>
2026-05-28 00:52:07 +0530

    Add files via upload

---
+++ Day 1/Identiy.java
@@ -0,0 +1,13 @@
+package myfirstproject;
+
+public class Identiy {
+
+	public static void main(String[] args) {
+		System.out.println("Name : Aman Kumar ");
+		System.out.println("College: Silicon university");
+		System.out.println("Department : CSE");
+		System.out.println("Hobbies : Cricket and Musics");
+
+	}
+
+}
